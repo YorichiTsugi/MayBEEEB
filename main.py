1a563a316654eb38a33aafb9aef051d3fc991159
YorichiTsugi <113590879+YorichiTsugi@users.noreply.github.com>
2022-10-06 19:56:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from random import randint
-spisok = []
+spis = []
 i = 0
 polz = 0
 otrz = 0
 print("Сколько чисел хош?")
 itachi = int(input())
-while len(spisok) != itachi:
+while len(spis) != itachi:
     a = randint (-100, 100)
     if a == 0:
         a = randint (-100, 100)
     else:
-        spisok.append(a)
+        spis.append(a)
 while i < len(spisok):
-    if spisok[i] > 0:
-        polz = polz + spisok[i]
+    if spis[i] > 0:
+        polz = polz + spis[i]
         i += 1
-    elif spisok[i] < 0:
-        otrz = otrz + spisok[i]
+    elif spis[i] < 0:
+        otrz = otrz + spis[i]
         i += 1
-print('Вот все твои циферки или числа, не знаю', spisok)
+print('Вот все твои циферки или числа, не знаю', spisok[:])
 print('А вот сумма всех положительных = ', polz)
 print('Ну и сумма отрицательных тоже = ', otrz)
